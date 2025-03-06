aa54b5b6aa15b27ba74143ef4c7b7152f90179b7
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-06 07:24:03 +0500

    fda

--- Arrays/practiseHashMap.js
+++ Arrays/practiseHashMap.js
@@ -90,14 +90,14 @@
 
 function sorting(arr) {
   let n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    // console.log(arr[i]);
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
-  }
+  // for (let i = 0; i < n - 1; i++) {
+  //   // console.log(arr[i]);
+  //   for (let j = 0; j < n - i - 1; j++) {
+  //     if (arr[j] > arr[j + 1]) {
+  //       [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  //     }
+  //   }
+  // }
   return arr;
 }
 
