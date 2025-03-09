b6c031911ff30911602f758ad7309b97a90ad790
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-10 01:38:08 +0500

    dsa

--- Arrays/practiseHashMap.js
+++ Arrays/practiseHashMap.js
@@ -2,17 +2,19 @@ const insertionSort = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     let curr = arr[i];
     let prev = i - 1;
+    console.log("this is prev 1 : ", prev);
 
     while (prev >= 0 && arr[prev] > curr) {
       arr[prev + 1] = arr[prev];
       prev--;
+      console.log("this is prev 2 : ", prev);
+      console.log("this is prev 2 : ", arr);
     }
-
+    console.log("this is prev 3: ", prev);
     arr[prev + 1] = curr;
-    console.log(arr);
   }
   return arr;
 };
 
-const arr = [5, 3, 8, 1, 2];
+const arr = [3, 4, 1, 8, 2, 6];
 console.log(insertionSort(arr));
