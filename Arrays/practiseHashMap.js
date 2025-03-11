5dae987a40eb98cb33204a7245c7ce273a567eb2
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-11 16:29:45 +0500

    sa

--- Arrays/practiseHashMap.js
+++ Arrays/practiseHashMap.js
@@ -1,20 +1,20 @@
-// const insertionSort = (arr) => {
-//   for (let i = 1; i < arr.length; i++) {
-//     let curr = arr[i];
-//     let prev = i - 1;
-//     console.log("this is prev 1 : ", prev);
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let curr = arr[i];
+    let prev = i - 1;
+    console.log("this is prev 1 : ", prev);
 
-//     while (prev >= 0 && arr[prev] > curr) {
-//       arr[prev + 1] = arr[prev];
-//       prev--;
-//       console.log("this is prev 2 : ", prev);
-//       console.log("this is prev 2 : ", arr);
-//     }
-//     console.log("this is prev 3: ", prev);
-//     arr[prev + 1] = curr;
-//   }
-//   return arr;
-// };
+    while (prev >= 0 && arr[prev] > curr) {
+      arr[prev + 1] = arr[prev];
+      prev--;
+      console.log("this is prev 2 : ", prev);
+      console.log("this is prev 2 : ", arr);
+    }
+    console.log("this is prev 3: ", prev);
+    arr[prev + 1] = curr;
+  }
+  return arr;
+};
 
-// const arr = [3, 4, 1, 8, 2, 6];
-// console.log(insertionSort(arr));
+const arr = [3, 4, 1, 8, 2, 6];
+console.log(insertionSort(arr));
