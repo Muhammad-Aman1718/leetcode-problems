cf34225050486e7ca9086040c2889ffd62d11b45
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-12 05:08:29 +0500

    s

--- Arrays/practiseHashMap.js
+++ Arrays/practiseHashMap.js
@@ -1,20 +1,37 @@
-const insertionSort = (arr) => {
-  for (let i = 1; i < arr.length; i++) {
-    let curr = arr[i];
-    let prev = i - 1;
-    console.log("this is prev 1 : ", prev);
+// const insertionSort = (arr) => {
+//   for (let i = 1; i < arr.length; i++) {
+//     let curr = arr[i];
+//     let prev = i - 1;
+//     console.log("this is prev 1 : ", prev);
 
-    while (prev >= 0 && arr[prev] > curr) {
-      arr[prev + 1] = arr[prev];
-      prev--;
-      console.log("this is prev 2 : ", prev);
-      console.log("this is prev 2 : ", arr);
-    }
-    console.log("this is prev 3: ", prev);
-    arr[prev + 1] = curr;
+//     while (prev >= 0 && arr[prev] > curr) {
+//       arr[prev + 1] = arr[prev];
+//       prev--;
+//       console.log("this is prev 2 : ", prev);
+//       console.log("this is prev 2 : ", arr);
+//     }
+//     console.log("this is prev 3: ", prev);
+//     arr[prev + 1] = curr;
+//   }
+//   return arr;
+// };
+
+// const arr = [3, 4, 1, 8, 2, 6];
+// console.log(insertionSort(arr));
+
+// Convert to any decimal number into binary form
+
+const decimalConverter = (num) => {
+  const arr = [];
+
+  while (num > 0) {
+    arr.unshift(num % 2);
+
+    let binary = num / 2;
+    num = ~~binary;
   }
-  return arr;
+  return arr.join(" ");
 };
 
-const arr = [3, 4, 1, 8, 2, 6];
-console.log(insertionSort(arr));
+const num = 5;
+console.log(decimalConverter(num));
