4597572a1f7f1cfb181ebe6f7316cdaaf081dbd7
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-17 00:52:58 +0500

    ds

--- Arrays/sorting-the-sentence.js
+++ Arrays/sorting-the-sentence.js
@@ -4,13 +4,6 @@
  */
 
 var sortSentence = function (s) {
-  //   let arr = s.split(" ");
-  //   let num = [];
-  //   for (let i = 0; i < arr.length; i++) {
-  //     num.push(arr[i].charAt(arr[i].length - 1));
-  //   }
-  //   console.log(num);
-
   return s
     .split(" ")
     .sort((a, b) => a[a.length - 1] - b[b.length - 1])
