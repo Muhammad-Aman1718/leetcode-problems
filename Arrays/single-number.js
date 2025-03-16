ab12fe872546b82f0184b0528de04b83703d8e76
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-17 00:57:11 +0500

    Single Number

--- Arrays/single-number.js
+++ Arrays/single-number.js
@@ -7,12 +7,12 @@ var singleNumber = function (nums) {
   let number = 0;
   for (let i = 0; i < nums.length; i++) {
     for (let j = 1; j < nums.length; j++) {
-      // if (nums[i] !== nums[j]) {
-      //   console.log(nums[i]);
-      //   number++;
-      // } else {
-      //   break;
-      // }
+      if (nums[i] !== nums[j]) {
+        console.log(nums[i]);
+        number++;
+      } else {
+        break;
+      }
     }
   }
   return number;
