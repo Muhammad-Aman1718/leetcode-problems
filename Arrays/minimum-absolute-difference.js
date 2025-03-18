1f90a4d2a3393f705f4553c10530b6786ee5e909
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-19 04:47:30 +0500

    Minimum absolute difference

--- Arrays/minimum-absolute-difference.js
+++ Arrays/minimum-absolute-difference.js
@@ -12,22 +12,25 @@ var minimumAbsDifference = function (arr) {
 
   for (let i = 0; i < arr.length; i++) {
     diffArr.push(arr[num] - arr[i]);
-    // if (arr[num] - arr[i] === diff) {
-    //   newArr.push([arr[i], arr[num]]);
-    // }
     num++;
   }
-
-  console.log(num);
-
-  console.log("this is diffaArr", diffArr);
+  diffArr.pop();
+  diffArr.sort((a, b) => a - b);
+  let num2 = 1;
+  for (let i = 0; i < arr.length; i++) {
+    diffArr.push(arr[num2] - arr[i]);
+    if (arr[num2] - arr[i] === diffArr[0]) {
+      newArr.push([arr[i], arr[num2]]);
+    }
+    num2++;
+  }
 
   return newArr;
 };
 
-let arr = [4, 2, 1, 3];
+// let arr = [4, 2, 1, 3];
 // let arr = [1,3,6,10,15]
 // let arr = [3, 8, -10, 23, 19, -4, -14, 27];
-// let arr = [40, 11, 26, 27, -20];
+let arr = [40, 11, 26, 27, -20];
 
 console.log(minimumAbsDifference(arr));
