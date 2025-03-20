da9dc2b17256c4c52efc8c564dce42a2af37c5db
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-21 02:17:46 +0500

    Average Salary Excluding the Minimum and Maximum Salary

---
+++ Arrays/average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+
+var average = function (salary) {
+  let minVal = Infinity,
+    maxVal = -Infinity,
+    total = 0;
+  for (let num of salary) {
+    if (num < minVal) minVal = num;
+    if (num > maxVal) maxVal = num;
+    total += num;
+  }
+
+  total -= minVal + maxVal;
+
+  let avg = total / (salary.length - 2);
+
+  return avg;
+};
+
+// let salary = [4000, 3000, 1000, 2000];
+let salary = [
+  25000, 48000, 57000, 86000, 33000, 10000, 42000, 3000, 54000, 29000, 79000,
+  40000,
+];
+console.log(average(salary));
