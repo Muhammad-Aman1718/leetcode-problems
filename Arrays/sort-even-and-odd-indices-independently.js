0b44793c460e6c3ceed3ffc51a266d4d19f66b98
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-03-23 18:45:17 +0500

    Sort even and odd indices independently

--- Arrays/sort-even-and-odd-indices-independently.js
+++ Arrays/sort-even-and-odd-indices-independently.js
@@ -3,6 +3,26 @@
  * @return {number[]}
  */
 
-var sortEvenOdd = function (nums) {};
+var sortEvenOdd = function (nums) {
+  let duplicateArr = [...nums];
+
+  let evenArr = duplicateArr
+    .filter((_, i) => i % 2 === 0)
+    .sort((a, b) => a - b);
+  let oddArr = duplicateArr.filter((_, i) => i % 2 === 1).sort((a, b) => b - a);
+
+  let evenIndex = 0,
+    oddIndex = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      duplicateArr[i] = evenArr[evenIndex++];
+    } else {
+      duplicateArr[i] = oddArr[oddIndex++];
+    }
+  }
+
+  return duplicateArr;
+};
 
 let nums = [4, 1, 2, 3];
+console.log(sortEvenOdd(nums));
