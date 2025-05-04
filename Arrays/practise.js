ac5ac8b06c24b4c5b987a928d2da2f5f70b2ead3
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-05-04 12:10:44 +0500

    asd

--- Arrays/practise.js
+++ Arrays/practise.js
@@ -1,11 +1,11 @@
 var sum = function (arr) {
   arr.sort((a, b) => b - a);
 
-  if (arr.length <= 1) {
-    return -1;
-  } else {
-    return arr[1];
-  }
+  // if (arr.length <= 1) {
+  //   return -1;
+  // } else {
+  //   return arr[1];
+  // }
 };
 
 let arr = [12, 35, 1, 10, 34, 1];
