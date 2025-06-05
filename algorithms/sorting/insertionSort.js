c3907585d09c96c56942cf5e784deb8506a0f198
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-05 09:06:13 +0500

    algo

---
+++ algorithms/sorting/insertionSort.js
@@ -0,0 +1,47 @@
+// // Insertion Sort
+// const insertionSort = (arr) => {
+//   for (let i = 1; i < arr.length; i++) {
+//     let key = arr[i];
+//     let j = i - 1;
+
+//     while (j >= 0 && arr[j] > key) {
+//       arr[j + 1] = arr[j]; // Shift greater element to the right
+//       j--;
+//     }
+
+//     arr[j + 1] = key; // Insert key in the correct position
+//   }
+//   return arr;
+// };
+
+// let arr = [4, 1, 2, 5, 3];
+// console.log(insertionSort(arr)); // Output: [1, 2, 3, 4, 5]
+
+
+
+
+
+// Insertion Sort
+const insertionSort = (arr) => {
+  // for (let i = 1; i < arr.length; i++) {
+  //   let key = arr[i];
+  //   let j = i - 1;
+
+  //   while (j >= 0 && arr[j] > key) {
+  //     arr[j + 1] = arr[j]; // Shift greater element to the right
+  //     j--;
+  //   }
+
+  //   arr[j + 1] = key; // Insert key in the correct position
+  // }
+  // return arr;
+
+
+
+  
+
+
+};
+
+let arr = [4, 1, 2, 5, 3];
+console.log(insertionSort(arr)); // Output: [1, 2, 3, 4, 5]
