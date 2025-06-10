ee711ae00d0b57ea928090909839157eb608b003
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-10 22:54:26 +0500

    Two Sum

---
+++ LeetCode/python/Two-Sum.py
@@ -0,0 +1,28 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target:int ) -> List[int]:
+
+
+
+
+
+
+
+
+
+# a = [1,3,4,5,6]
+
+
+
+a = "Hello"
+b = 10
+c = 11.3
+d = ("Geeks", "for", "Geeks")
+e = ["Geeks", "for", "Geeks"]
+f = {"Geeks": 1, "for":2, "Geeks":3}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
