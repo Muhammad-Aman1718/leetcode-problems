e544b079ec83f9e2cfa360757dba6b68ec361cd9
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-11 00:21:07 +0500

    Two Sum

--- LeetCode/python/Two-Sum.py
+++ LeetCode/python/Two-Sum.py
@@ -2,15 +2,7 @@
 #     def twoSum(self, nums: List[int], target:int ) -> List[int]:
 
 
-
-
-
-
-
-
-
 # a = [1,3,4,5,6]
-
 
 
 a = "Hello"
@@ -18,7 +10,7 @@ b = 10
 c = 11.3
 d = ("Geeks", "for", "Geeks")
 e = ["Geeks", "for", "Geeks"]
-f = {"Geeks": 1, "for":2, "Geeks":3}
+f = {"Geeks": 1, "for": 2, "Geeks": 3}
 
 print(type(a))
 print(type(b))
