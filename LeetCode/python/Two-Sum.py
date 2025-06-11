d02ff5a5f117c144456a176f4c01c44fcefd2f6f
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-12 01:45:44 +0500

    fdsa

--- LeetCode/python/Two-Sum.py
+++ LeetCode/python/Two-Sum.py
@@ -5,16 +5,34 @@
 # a = [1,3,4,5,6]
 
 
-a = "Hello"
-b = 10
-c = 11.3
-d = ("Geeks", "for", "Geeks")
-e = ["Geeks", "for", "Geeks"]
-f = {"Geeks": 1, "for": 2, "Geeks": 3}
+# a = "Hello"
+# b = 10
+# c = 11.3
+# d = ("Geeks", "for", "Geeks")
+# e = ["Geeks", "for", "Geeks"]
+# f = {"Geeks": 1, "for": 2, "Geeks": 3}
 
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
+# print(f" this is string {type(a)}")
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+
+# a = list((input("enter your num : ")))
+# b = tuple(input("enter your num : "))
+# words_str = input("Enter a list of words, separated by spaces: ")
+# words = {word: len(word) for word in words_str.split()}
+
+
+# print(f" {a} type of {type(a)} ")
+# print(f" {b} type of {type(b)} ")
+# print(f" {words} type of {type(words)} ")
+
+
+
+word = 'geeksforgeek'
+
+# Splitting at ':'
+print(word.split('s'))
