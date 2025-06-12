d7b95dcbcbc94e28c267c9f7bf130c23abcba862
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-12 11:13:58 +0500

    Wrorking

--- LeetCode/python/Two-Sum.py
+++ LeetCode/python/Two-Sum.py
@@ -1,38 +1,11 @@
+# from sys import stdin, stdout
+
 # class Solution:
 #     def twoSum(self, nums: List[int], target:int ) -> List[int]:
 
 
-# a = [1,3,4,5,6]
+base = int(input("Enter your base : "))
+power = int(input("Enter your power : "))
 
-
-# a = "Hello"
-# b = 10
-# c = 11.3
-# d = ("Geeks", "for", "Geeks")
-# e = ["Geeks", "for", "Geeks"]
-# f = {"Geeks": 1, "for": 2, "Geeks": 3}
-
-# print(f" this is string {type(a)}")
-# print(type(b))
-# print(type(c))
-# print(type(d))
-# print(type(e))
-# print(type(f))
-
-
-# a = list((input("enter your num : ")))
-# b = tuple(input("enter your num : "))
-# words_str = input("Enter a list of words, separated by spaces: ")
-# words = {word: len(word) for word in words_str.split()}
-
-
-# print(f" {a} type of {type(a)} ")
-# print(f" {b} type of {type(b)} ")
-# print(f" {words} type of {type(words)} ")
-
-
-
-word = 'geeksforgeek'
-
-# Splitting at ':'
-print(word.split('s'))
+print(f"exponent : {base ** power} ")
+print(f"floor : {base // power} ")
