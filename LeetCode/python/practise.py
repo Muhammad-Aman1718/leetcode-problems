14c75e32c8e54d2e2c91728cc028c86753a8f994
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-16 13:06:37 +0500

    dsa

--- LeetCode/python/practise.py
+++ LeetCode/python/practise.py
@@ -1,18 +1,34 @@
-def primeNum(a: int):
+# Prime number finder
+# def primeNum(a: int):
 
-    if (a == 2) or (a == 3):
-        return print("this is prime number")
+#     if (a == 2) or (a == 3):
+#         return print("this is prime number")
+#     i = 2
+#     while i <= a:
+#         if a / i == 1:
+#             return print("this is prime number")
+#         if a % i == 0:
+#             return print("this is not prime number")
+#         i += 1
 
-    i = 2
-
-    while i <= a:
-        if a / i == 1:
-            return print("this is prime number")
-        if a % i == 0:
-            return print("this is not prime number")
-        i += 1
+# num = int(input("Enter your Number : "))
+# print(primeNum(num))
 
 
-num = int(input("Enter your Number : "))
+# def fun():
+#     s = 2
+#     return s
 
-print(primeNum(num))
+# print(fun())
+
+
+def fun():
+
+    yield 1
+    yield 2
+    yield 3
+    # return 
+
+
+for value in fun():
+    print(value)
