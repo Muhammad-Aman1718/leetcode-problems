aeee320cc3a402ca3cb79d9ec827f57c9cb6ae8c
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-06-26 22:27:33 +0500

    'Working

--- LeetCode/python/practise.py
+++ LeetCode/python/practise.py
@@ -1,9 +1,9 @@
 a = list(map(int, input().split()))
-b = list(map(str, input().split()))
-query = list(map(int, input().split()))
-dict = {}
-for i in range(len(a)):
-    dict[a[i]] = b[i]
+# b = list(map(str, input().split()))
+# query = list(map(int, input().split()))
+# dict = {}
+# for i in range(len(a)):
+#     dict[a[i]] = b[i]
         
 # ans = []
 # for key in range(len(query)):
