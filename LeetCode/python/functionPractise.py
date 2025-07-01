b52452ac1e8533591b33452afa698aeab5a55494
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-07-02 00:27:36 +0500

    fdsa

--- LeetCode/python/functionPractise.py
+++ LeetCode/python/functionPractise.py
@@ -11,3 +11,19 @@
 #     return a + b
 
 
+# Recursion
+
+
+a = input("Enter a number to find its factorial: ")
+
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.casefold())
+print(a.center(500))
+print(a.islower())
+print(a.isupper())
+print(a.title())
+print(a.upper())
+print(dir(str))  # sab string methods list kar dega
+
