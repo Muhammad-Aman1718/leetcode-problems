df1e7bade9175f639e3caeb513d636c64625ee19
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-07-02 09:23:51 +0500

    Working on python

--- LeetCode/python/functionPractise.py
+++ LeetCode/python/functionPractise.py
@@ -14,16 +14,21 @@
 # Recursion
 
 
-a = input("Enter a number to find its factorial: ")
+# a = input("Enter a number to find its factorial: ")
 
-print(a.upper())
-print(a.lower())
-print(a.capitalize())
-print(a.casefold())
-print(a.center(500))
-print(a.islower())
-print(a.isupper())
-print(a.title())
-print(a.upper())
-print(dir(str))  # sab string methods list kar dega
-
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+# print(a.casefold())
+# print(a.center(500))
+# print(a.islower())
+# print(a.isupper())
+# print(a.title())
+# print(a.upper())
+print("this is str", dir(str))  # sab string methods list kar dega
+print("this is int", dir(int))  # sab string methods list kar dega
+print("this float ", dir(float))  # sab string methods list kar dega
+print("this is list ", dir(list))  # sab string methods list kar dega
+print("this is dictionary", dir(dict))  # sab string methods list kar dega
+print("this is set ", dir(set))  # sab string methods list kar dega
+print("this is tuple", dir(tuple))  # sab string methods list kar dega
