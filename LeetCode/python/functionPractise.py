f304a06ebccdad946016378d657e449987a09754
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-07-11 15:19:53 +0500

    'fdsa

--- LeetCode/python/functionPractise.py
+++ LeetCode/python/functionPractise.py
@@ -10,17 +10,3 @@
 # def add(a: int, b: int) -> int:
 #     return a + b
 
-
-# *arugs and **kwargs
-def add(*args: int, **kwargs: int) -> int:
-    total = 0
-    for arg in args:
-        total += arg
-    for key, value in kwargs.items():
-        total += value
-    return total
-
-
-# Example usage
-result = add(1, 2, 3, x=4, y=5)
-print(result)  # Output: 15
