63088f4b4b638a42b8a3068bc5597f5c73974bd4
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-07-30 02:46:06 +0500

    practise on python

--- pracitse/python/functionPractise.py
+++ pracitse/python/functionPractise.py
@@ -1,2 +1,13 @@
+# *args example
+def fun(*args: int) -> int:
+    return sum(args)
 
-print("Hello  World")
+print(fun(1, 2, 3, 4)) 
+print(fun(5, 10, 15))   
+
+# **kwargs example
+def run(**kwargs: int) -> None:
+    for k, val in kwargs.items():
+        print(k, val)
+
+run(a=1, b=2, c=3)
