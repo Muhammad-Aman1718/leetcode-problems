4d0c786fd33139e4ca5789941a608a50d157fe78
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-08-03 02:13:49 +0500

    p

---
+++ pracitse/OOP.py
@@ -0,0 +1,26 @@
+# class Car:
+#     def __init__(self, brand, color):
+#         self.brand = brand
+#         self.color = color
+
+#     def drive(self):
+#         print(f"{self.color} {self.brand} is driving.")
+
+
+# # Objects
+# car1 = Car("Toyota", "Red")
+# car2 = Car("Honda", "Blue")
+
+# car1.drive()  # Red Toyota is driving.
+# car2.drive()  # Blue Honda is driving.
+
+# this is
+
+
+class Student:
+    name = "aman"
+
+
+s1 = Student()
+s2 = Student()
+print(s1, s2.name)
