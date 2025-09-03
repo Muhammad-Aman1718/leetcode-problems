1fdfc47e7dd58fbf4a469d03113cf5cac7d19857
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-03 15:35:27 +0500

    Distribute Candies

---
+++ LeetCode/Arrays/python/distribute_Candies.py
@@ -0,0 +1,59 @@
+# Distribute Candies
+
+
+# Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
+
+# The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+
+# Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+
+
+# Example 1:
+
+# Input: candyType = [1,1,2,2,3,3]
+# Output: 3
+# Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
+
+# Example 2:
+
+# Input: candyType = [1,1,2,3]
+# Output: 2
+# Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types [1,2], [1,3], or [2,3], she still can only eat 2 different types.
+
+# Example 3:
+
+# Input: candyType = [6,6,6,6]
+# Output: 1
+# Explanation: Alice can only eat 4 / 2 = 2 candies. Even though she can eat 2 candies, she only has 1 type.
+
+
+class Solution:
+    def distributeCandies(self, candyType: list[int]) -> int:
+
+        maximum = len(candyType) // 2
+        unique = len(set(candyType))
+        return min(maximum, unique)
+    
+    
+    # THis is my made 
+
+        for i in candyType:
+            if i in dictoinary:
+                dictoinary[i] += 1
+            else:
+                dictoinary[i] = 1
+
+        if len(dictoinary) == 1:
+            return 1
+        elif len(dictoinary) < maximum:
+            return len(dictoinary)
+        else:
+            return maximum
+
+
+obj = Solution()
+# candyType = [1, 1, 1, 1]
+candyType = [1, 1, 1, 1, 2, 2, 2, 3, 3, 3]
+
+
+print(obj.distributeCandies(candyType))
---
+++ LeetCode/Arrays/python/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+len(dictoinary) < maximum
