ab63ae2522e7d87e8276c78b01f8df320b5f53d2
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-03 10:49:57 +0500

    Contains Duplicate

---
+++ LeetCode/Arrays/python/contains_Duplicate.py
@@ -0,0 +1,47 @@
+# Contains Duplicate
+
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Explanation:
+# The element 1 occurs at the indices 0 and 3.
+
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Explanation:
+# All elements are distinct.
+
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+
+        frequency: dict[int, int] = {}
+        count = 1
+
+        for i in range(len(nums)):
+            if nums[i] in frequency:
+                frequency[nums[i]] += 1
+            else:
+                frequency[nums[i]] = count
+            if frequency[nums[i]] > 1:
+                return True
+
+        return False
+
+
+obj = Solution()
+nums = [1, 2, 3, 1]
+# nums = [1, 2, 3, 4]
+print(obj.containsDuplicate(nums))
