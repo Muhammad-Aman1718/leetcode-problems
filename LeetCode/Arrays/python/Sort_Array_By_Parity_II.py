334bc803d2ac3009f9df4ee34eb7289e4ae40c25
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-16 14:22:17 +0500

    Sort Array By Parity II

---
+++ LeetCode/Arrays/python/Sort_Array_By_Parity_II.py
@@ -0,0 +1,54 @@
+# Sort Array By Parity II
+
+# Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+# Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+# Return any answer array that satisfies this condition.
+
+
+# Example 1:
+
+# Input: nums = [4,2,5,7]
+# Output: [4,5,2,7]
+# Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+# Example 2:
+
+# Input: nums = [2,3]
+# Output: [2,3]
+
+
+class Solution:
+    def sortArrayByParityII(self, nums: list[int]) -> list[int]:
+
+        evenArray = [x for x in nums if x % 2 == 0]
+
+        oddArray = [x for x in nums if x % 2 != 0]
+
+        sortedArray: list[int] = []
+        i, j = 0, 0
+
+        # alternate even and odd
+        while i < len(evenArray) and j < len(oddArray):
+            sortedArray.append(evenArray[i])
+            sortedArray.append(oddArray[j])
+            i += 1
+            j += 1
+
+        # add remaining evens
+        while i < len(evenArray):
+            sortedArray.append(evenArray[i])
+            i += 1
+
+        # add remaining odds
+        while j < len(oddArray):
+            sortedArray.append(oddArray[j])
+            j += 1
+
+        return sortedArray
+
+
+obj = Solution()
+nums = [4, 2, 5, 7]
+print(obj.sortArrayByParityII(nums))
