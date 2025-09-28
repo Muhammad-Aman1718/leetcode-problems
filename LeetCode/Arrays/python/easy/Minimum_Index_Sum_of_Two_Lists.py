e8421bb060c1d0e6178bc901894c90d2b7b2b012
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-29 03:11:09 +0500

    Minimum Index Sum of Two Lists

---
+++ LeetCode/Arrays/python/easy/Minimum_Index_Sum_of_Two_Lists.py
@@ -0,0 +1,54 @@
+# Minimum Index Sum of Two Lists
+
+# Given two arrays of strings list1 and list2, find the common strings with the least index sum.
+# A common string is a string that appeared in both list1 and list2.
+# A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.
+# Return all the common strings with the least index sum. Return the answer in any order.
+
+
+# Example 1:
+
+# Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+# Output: ["Shogun"]
+# Explanation: The only common string is "Shogun".
+
+# Example 2:
+
+# Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+# Output: ["Shogun"]
+# Explanation: The common string with the least index sum is "Shogun" with index sum = (0 + 1) = 1.
+
+# Example 3:
+
+# Input: list1 = ["happy","sad","good"], list2 = ["sad","happy","good"]
+# Output: ["sad","happy"]
+# Explanation: There are three common strings:
+# "happy" with index sum = (0 + 1) = 1.
+# "sad" with index sum = (1 + 0) = 1.
+# "good" with index sum = (2 + 2) = 4.
+# The strings with the least index sum are "sad" and "happy".
+
+
+class Solution:
+    def findRestaurant(self, list1: list[str], list2: list[str]) -> list[str]:
+
+        leastIndex: dict[str, int] = {}
+        result: list[str] = []
+
+        for i in range(len(list1)):
+            if list1[i] in list2:
+                leastIndex[list1[i]] = i + list2.index(list1[i])
+        minValue = min(leastIndex.values())
+        for i in leastIndex:
+            if leastIndex[i] == minValue:
+                result.append(i)
+
+        return result
+
+
+obj = Solution()
+list1 = ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+list2 = ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+# list1 = ["happy", "sad", "good"]
+# list2 = ["sad", "happy", "good"]
+print(obj.findRestaurant(list1, list2))
