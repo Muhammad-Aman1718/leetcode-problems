efb483759963f585b2bd0ddd1e8b9796c6cca3a8
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-09-29 00:39:32 +0500

    Count Pairs Of Similar Strings

---
+++ LeetCode/Arrays/python/easy/Count_Pairs_Of_Similar_Strings.py
@@ -0,0 +1,56 @@
+# Count Pairs Of Similar Strings
+
+# You are given a 0-indexed string array words.
+
+# Two strings are similar if they consist of the same characters.
+
+# For example, "abca" and "cba" are similar since both consist of characters 'a', 'b', and 'c'.
+# However, "abacba" and "bcfd" are not similar since they do not consist of the same characters.
+# Return the number of pairs (i, j) such that 0 <= i < j <= word.length - 1 and the two strings words[i] and words[j] are similar.
+
+
+# Example 1:
+
+# Input: words = ["aba","aabb","abcd","bac","aabc"]
+# Output: 2
+# Explanation: There are 2 pairs that satisfy the conditions:
+# - i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'.
+# - i = 3 and j = 4 : both words[3] and words[4] only consist of characters 'a', 'b', and 'c'.
+
+# Example 2:
+
+# Input: words = ["aabb","ab","ba"]
+# Output: 3
+# Explanation: There are 3 pairs that satisfy the conditions:
+# - i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'.
+# - i = 0 and j = 2 : both words[0] and words[2] only consist of characters 'a' and 'b'.
+# - i = 1 and j = 2 : both words[1] and words[2] only consist of characters 'a' and 'b'.
+
+# Example 3:
+
+# Input: words = ["nba","cba","dba"]
+# Output: 0
+# Explanation: Since there does not exist any pair that satisfies the conditions, we return 0.
+
+
+class Solution:
+    def similarPairs(self, words: list[str]) -> int:
+
+        sortedArray: list[str] = []
+        freq: dict[str, int] = {}
+        count = 0
+        for i in words:
+            sortedArray.append("".join(set(sorted(i))))
+
+        for i in sortedArray:
+            freq[i] = freq.get(i, 0) + 1
+        for i in freq:
+            if freq[i] > 1:
+                count += (freq[i] * (freq[i] - 1)) // 2
+        return count
+
+
+obj = Solution()
+words = ["aba", "aabb", "abcd", "bac", "aabc"]
+# words = ["aabb", "ab", "ba"]
+print(obj.similarPairs(words))
