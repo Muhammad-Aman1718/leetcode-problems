baeac50c8fc4b50872d04330c3ab8d8e36348fff
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-04 13:35:49 +0500

    Unique Morse Code Words

---
+++ LeetCode/neetcode_250_question_list/Arrays_&_Hashing/medium/Subarray_Sum_Equals_K.py
@@ -0,0 +1,31 @@
+# Subarray Sum Equals K
+
+# Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+# A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+# Example 1:
+
+# Input: nums = [1,1,1], k = 2
+# Output: 2
+
+# Example 2:
+
+# Input: nums = [1,2,3], k = 3
+# Output: 2
+
+
+
+class Solution:
+    def subarraySum(self, nums:list[int], k:int) -> int:
+        
+        
+        
+        
+        
+        
+obj = Solution()
+nums = [1,1,1]
+k = 2
+print(obj.subarraySum(nums,k))
