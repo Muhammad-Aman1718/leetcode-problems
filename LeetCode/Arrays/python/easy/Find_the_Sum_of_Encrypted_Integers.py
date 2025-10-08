2fc376d4d23fedba5428cb163166dc89ccf92965
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-08 18:46:09 +0500

    Find the Sum of Encrypted Integers

---
+++ LeetCode/Arrays/python/easy/Find_the_Sum_of_Encrypted_Integers.py
@@ -0,0 +1,37 @@
+# Find the Sum of Encrypted Integers
+
+
+# You are given an integer array nums containing positive integers. We define a function encrypt such that encrypt(x) replaces every digit in x with the largest digit in x. For example, encrypt(523) = 555 and encrypt(213) = 333.
+
+# Return the sum of encrypted elements.
+
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: 6
+# Explanation: The encrypted elements are [1,2,3]. The sum of encrypted elements is 1 + 2 + 3 == 6.
+
+# Example 2:
+
+# Input: nums = [10,21,31]
+# Output: 66
+# Explanation: The encrypted elements are [11,22,33]. The sum of encrypted elements is 11 + 22 + 33 == 66.
+
+
+class Solution:
+    def sumOfEncryptedInt(self, nums: list[int]) -> int:
+
+        totalSum = 0
+
+        for i in range(len(nums)):
+            stringI = str(nums[i])
+            maxValue = max(stringI)
+            stringI = maxValue * len(stringI)
+            totalSum += int(stringI)
+        return totalSum
+
+
+obj = Solution()
+nums = [10, 21, 31]
+print(obj.sumOfEncryptedInt(nums))
