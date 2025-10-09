9e90488fc3688a5cee08cea36de83d1e1b62a511
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-09 14:42:30 +0500

    Valid Palindrome

--- pracitse/OOP.py
+++ pracitse/OOP.py
@@ -1,4 +1,28 @@
-# players = [[0] * 11 for _ in range(10)]
-# print(players)
-a, b, c = 1, 4, 5
-print(a,b,c)
+class Solution:
+    def is_possible_to_get_seats(self, n: int, m: int, seats: list[int]) -> bool:
+        # code here
+        for i in range(m):
+            if (
+                seats[i] == 0
+                and (i == 0 or seats[i - 1] == 0)
+                and (i == m - 1 or seats[i + 1] == 0)
+            ):
+                seats[i] = 1  # ✅ mark seat as occupied
+                n -= 1
+                if n == 0:  # ✅ early stop if all seated
+                    return True
+
+        return n <= 0
+
+
+obj = Solution()
+n = 2
+m = 7
+seats = [0, 0, 1, 0, 0, 0, 1]
+# n = 1
+# m = 3
+# seats = [0, 1, 0]
+n = 2
+m = 2
+seats = [0, 0]
+print(obj.is_possible_to_get_seats(n, m, seats))
