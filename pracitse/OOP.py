8050121341768643c9237fb089a3e4564606b0c7
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-09 23:25:40 +0500

    Remove Duplicates from Sorted List

--- pracitse/OOP.py
+++ pracitse/OOP.py
@@ -1,28 +1,20 @@
 class Solution:
-    def is_possible_to_get_seats(self, n: int, m: int, seats: list[int]) -> bool:
+    def is_possible_to_get_seats(self, arr: list[int]) -> bool:
         # code here
-        for i in range(m):
-            if (
-                seats[i] == 0
-                and (i == 0 or seats[i - 1] == 0)
-                and (i == m - 1 or seats[i + 1] == 0)
-            ):
-                seats[i] = 1  # ✅ mark seat as occupied
-                n -= 1
-                if n == 0:  # ✅ early stop if all seated
-                    return True
-
-        return n <= 0
+        result = []
+        freq: dict[int, int] = {}
+        for i in arr:
+            freq[i] = freq.get(i, 0) + 1
+        print(freq)
+        t = max(freq.values())
+        print(t)
+        for i in freq:
+            if freq[i] == t:
+                result.append(i)
+        return min(result)
 
 
 obj = Solution()
-n = 2
-m = 7
-seats = [0, 0, 1, 0, 0, 0, 1]
-# n = 1
-# m = 3
-# seats = [0, 1, 0]
-n = 2
-m = 2
-seats = [0, 0]
-print(obj.is_possible_to_get_seats(n, m, seats))
+seats = [2, 2, 1, 1, 0, 0, ]
+
+print(obj.is_possible_to_get_seats(seats))
