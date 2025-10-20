6dd71aa614de004b6a7bb1dccb211e796f915457
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-20 10:09:54 +0500

    Binary Tree Inorder Traversal

---
+++ LeetCode/neetcode_250_question_list/Trees/easy/Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,68 @@
+# Binary Tree Inorder Traversal
+
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+# Example 1:
+
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+
+
+# Example 2:
+
+# Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+# Output: [4,2,6,5,7,1,3,9,8]
+
+# Example 3:
+
+# Input: root = []
+# Output: []
+
+# Example 4:
+
+# Input: root = [1]
+# Output: [1]
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.left: "TreeNode | None" = None
+        self.right: "TreeNode | None" = None
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.left.right.left = TreeNode(6)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(8)
+root.right.right.left = TreeNode(9)
+
+
+# inorder traversal
+# Rule for inorder traversal: Left -> Root -> Right
+class Solution:
+    def __init__(self) -> None:
+        self.result: list[int] = []
+
+    def inOrder(self, root: "TreeNode |None "):
+        if root == None:
+            return
+
+        self.inOrder(root.left)
+        self.result.append(root.val)
+        self.inOrder(root.right)
+
+    def inorderTraversal(self, root: "TreeNode") -> list[int]:
+
+        self.inOrder(root)
+        return self.result
+
+
+obj = Solution()
+print(obj.inorderTraversal(root))
