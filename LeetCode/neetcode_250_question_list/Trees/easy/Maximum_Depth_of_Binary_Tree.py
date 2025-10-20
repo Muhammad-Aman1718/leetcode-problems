90153e046c7e7f7eeed595c2b0ea6ed01a71d746
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-20 13:54:24 +0500

    Binary Tree Postorder Traversal

--- LeetCode/neetcode_250_question_list/Trees/easy/Maximum_Depth_of_Binary_Tree.py
+++ LeetCode/neetcode_250_question_list/Trees/easy/Maximum_Depth_of_Binary_Tree.py
@@ -18,13 +18,26 @@
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.left: "TreeNode | None" = None
+        self.right: "TreeNode | None" = None
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.left.right.left = TreeNode(6)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(8)
+root.right.right.left = TreeNode(9)
+
+
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+    def maxDepth(self, root: "TreeNode | None") -> int:
         # Base Case
         if root == None:
             return 0
