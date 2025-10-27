2b06ffab5fea11cd532553629659f4bd07dda7d0
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-10-27 05:06:01 +0500

    A. Lever

---
+++ pracitse/OOP.py
@@ -0,0 +1,20 @@
+# Function to return double of n
+def double(n: int):
+    return n**2
+
+
+# Using map to double all numbers
+# numbers = [5, 6, 7, 8]
+# result = map(double, numbers)
+# print(list(result))
+
+
+# def fun(nums):
+#     even = [x for x in nums if x % 2 == 0]
+#     print(even)
+#     odd = [x for x in nums if x % 2 != 0]
+#     print(odd   )
+#     return even + odd
+
+
+# print(fun([3, 1, 2, 4]))
