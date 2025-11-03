3e0c47d486d84602f2e83cfedd2a1b40a273b8d4
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-11-03 19:21:29 +0500

    Score of a String

--- LeetCode/Strings/easy/Score_of_a_String.py
+++ LeetCode/Strings/easy/Score_of_a_String.py
@@ -21,7 +21,7 @@
 
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        total = 0
+        total: int = 0
         for i in range(len(s) - 1):
             total += abs(ord(s[i]) - ord(s[i + 1]))
         return total
