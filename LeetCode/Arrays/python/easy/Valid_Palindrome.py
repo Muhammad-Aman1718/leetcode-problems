088bfd4ec5146803757a90b549d94a7cb5dc0e00
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-12-05 22:29:58 +0500

    Valid Palindrome

--- LeetCode/Arrays/python/easy/Valid_Palindrome.py
+++ LeetCode/Arrays/python/easy/Valid_Palindrome.py
@@ -28,12 +28,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        newString = ""
-        for i in s:
-            if i.isalnum():
-                newString += i.lower()
+        l, r = 0, len(s) - 1
 
-        return newString == newString[::-1]
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+
+            while l < r and not s[r].isalnum():
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
 
 
 obj = Solution()
