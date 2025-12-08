f36802d7d307b6ce81865733dc4978b98e77161c
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-12-08 23:05:50 +0500

    Sort Vowels in a String

---
+++ LeetCode/Strings/medium/Sort_Vowels_in_a_String.py
@@ -0,0 +1,40 @@
+# Sort Vowels in a String
+
+# Given a 0-indexed string s, permute s to get a new string t such that:
+
+# All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+# The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+# Return the resulting string.
+
+# The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+
+
+# Example 1:
+
+# Input: s = "lEetcOde"
+# Output: "lEOtcede"
+# Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+
+# Example 2:
+
+# Input: s = "lYmpH"
+# Output: "lYmpH"
+# Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+
+
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels = set("aeiouAEIOU")
+        string = list(s)
+
+        vowel_indices = [i for i, ch in enumerate(string) if ch in vowels]
+        sorted_vowels = sorted((string[i] for i in vowel_indices), key=ord)
+        for idx, val in zip(vowel_indices, sorted_vowels):
+            string[idx] = val
+
+        return "".join(string)
+
+
+obj = Solution()
+s = "lEetcOde"
+print(obj.sortVowels(s))
