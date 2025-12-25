1aee530f6f95f3e7167da160365c4c032b3dfb60
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-12-25 16:16:30 +0500

    Longest Repeating Character Replacement

--- LeetCode/Strings/medium/Longest_Repeating_Character_Replacement.py
+++ LeetCode/Strings/medium/Longest_Repeating_Character_Replacement.py
@@ -26,16 +26,17 @@ class Solution:
         count = {}
         res = 0
         left = 0
-        max_freq = 0
+        maxFreq = 0
 
         for right in range(len(s)):
-            count[s[right]] = 1 + count.get(s[right], 0)
+            count[s[right]] = count.get(s[right], 0) + 1
 
-            max_freq = max(max_freq, count[s[right]])
+            maxFreq = max(maxFreq, count[s[right]])
 
-            while (right - left + 1) - max_freq > k:
+            while (right - left + 1) - maxFreq > k:
                 count[s[left]] -= 1
                 left += 1
+
             res = max(res, right - left + 1)
 
         return res
