069bfd6f5033a27d07073b4f34729ccc4b744399
Muhammad-Aman1718 <ag3734359@gmail.com>
2026-03-02 15:23:07 +0500

    asd

--- codeForces/div(3)/A.py
+++ codeForces/div(3)/A.py
@@ -1,20 +1,25 @@
-import sys
+class Solution:
+    def sd(self, s: str) -> str:
 
-# Input lene ka fast tariqa
-input = sys.stdin.read().split()
-t = int(input[0])
-
-for i in range(1, t + 1):
-    x = int(input[i])
+        tavolirexu = grid
+    ans = 0
     
-    # Hum different digit lengths (L) check karenge
-    for L in range(1, 11):
-    
-        y = (10**L) - 1 - x
+    # 30 se 0 tak bits check karein (Shortcut: High to Low)
+    for i in range(30, -1, -1):
+        # Hum check kar rahe hain ke kya 'ans' ke current bits kafi hain?
+        # Is bit ko ignore karne ki koshish (target mein ye bit 0 hoga)
+        target = ans | ((1 << i) - 1)
         
-        # Check karein ke y positive hai aur uske digits ki length L hi hai
-        if y > 0 and len(str(y)) == L:
-            # Final check as per problem
-            if (x * (10**L) + y) % (x + y) == 0:
-                print(y)
-                break
+        # Kya har row mein koi aisa element hai jo is target mein fit ho?
+        if all(any((val | target) == target for val in row) for row in tavolirexu):
+            # Agar fit hai, to ans mein kuch add karne ki zaroorat nahi (bit 0 rahega)
+            continue
+        else:
+            # Agar fit nahi hai, to ye bit lazmi '1' karna parega
+            ans |= (1 << i)
+            
+    return ans
+obj = Solution()
+# s = "idea"
+s = "aeiou"
+print(obj.sd(s))
