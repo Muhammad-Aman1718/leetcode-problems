63ab0e382d0c342041b5a334ba692ea6fcc89cb3
Muhammad-Aman1718 <ag3734359@gmail.com>
2026-03-13 23:09:16 +0500

    practise

--- pracitse/OOP.py
+++ pracitse/OOP.py
@@ -1,7 +1,10 @@
-list1 = [3, 4, 5, 6, 7]
-for i in list1:
-    print(list1)
-    list1.remove(i)
-    print(list1)
+# list1 = [3, 4, 5, 6, 7]
+# for i in list1:
+#     print(list1)
+#     list1.remove(i)
+#     print(list1)
 
-print(list1)
+# print(list1)
+
+
+print(~-5)
