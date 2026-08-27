7ee6a9323c46a69eba281e6ddb351848e6a6ff5a
Muhammad-Aman1718 <ag3734359@gmail.com>
2026-08-27 22:51:10 +0200

    fsada

--- LeetCode/bitManipulation/easy/Set_Mismatch.py
+++ LeetCode/bitManipulation/easy/Set_Mismatch.py
@@ -20,6 +20,7 @@
 class Solution:
     def findErrorNums(self, nums: list[int]) -> list[int]:
         arr = []
+        adf=4
         for i in range(0, nums[-1]):
             if nums[i] == nums[j]:
                 arr.append(nums[i])
